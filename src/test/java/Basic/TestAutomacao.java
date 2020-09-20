7db7828c180b778d6e7c5c9e6938e4a1b1d50054
Juliana carla <julianacarlas>
2020-09-20 16:14:11 -0300

    alterações

--- src/test/java/Basic/TestAutomacao.java
+++ src/test/java/Basic/TestAutomacao.java
@@ -14,7 +14,6 @@ public class TestAutomacao extends BaseTest {
         navegador.findElement(By.name("form_senha")).sendKeys("123456");
         navegador.findElement(By.name("form_nome")).sendKeys("Aluno Cadastrado");
         navegador.findElement(By.xpath("//tr[7]/td/input")).click();
-        assertEquals("Aluno Cadastrado","Aluno Cadastrado");
         navegador.findElement(By.linkText("Clique aqui")).click();
         navegador.findElement(By.linkText("Apagar")).click();
 
